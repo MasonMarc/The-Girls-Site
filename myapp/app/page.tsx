108ae4b769fc930e0dfbddc0634561490e635003
MasonMarc <masonmarcantel@gmail.com>
2023-03-20 15:11:02 -0400

    minor css changes

--- myapp/app/page.tsx
+++ myapp/app/page.tsx
@@ -4,12 +4,13 @@
 export default function HomePage() {
   return (
     <div className="landing">
-      <div className='row'>
-        <div className="col-md-auto" id="JP"><img src="assets/3G3A7641.jpg" width={460}></img></div>
-        <div className="col-md-auto" id="Kait"><img src="assets/3G3A7830.jpg" width={460}></img></div>
-        <div className="col-md-auto" id="Sky"><img src="assets/3G3A8069.jpg" width={460}></img></div>
-        <div className="col-md-auto" id="Noah"><img src="assets/3G3A7883.jpg" width={460}></img></div>
+      <div className='row ms-1'>
+        <div className="col-md-auto" id="JP"><img src="assets/3G3A7641.jpg" width={450} height={680}></img></div>
+        <div className="col-md-auto" id="Kait"><img src="assets/3G3A7830.jpg" width={450} height={680}></img></div>
+        <div className="col-md-auto" id="Sky"><img src="assets/3G3A8069.jpg" width={450} height={680}></img></div>
+        <div className="col-md-auto" id="Noah"><img src="assets/3G3A7883.jpg" width={450} height={680}></img></div>
       </div>
+      <div id="blank"></div>
     </div >
   )
 }
